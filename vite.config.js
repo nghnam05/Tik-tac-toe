ef8fde9c229d8cc528815b366ce6c8a66f33f5c4
Hoai Nam <nhoaiinamm05@gmail.com>
2025-08-04 11:20:16 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tictactoe-react/",
+  base: "/Tik-tac-toe/",
   plugins: [tailwindcss(), react()],
 });
